db7c93e0893838128a9e6d3f18f9f8e16076d74d
Abeerahmad566 <abeerahmad389@gmail.com>
2023-11-01 17:31:10 +0500

    first commit

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,62 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
+  //   @Get()
+  //   findAll(@Query() paginationQuery) {
+  //     //@Res() response
+  //     // response.status(200).send('This action return all coffees');
+  //     const { limit, offset } = paginationQuery;
+
+  //     return `This action return all coffees.Limit: ${limit}, offset: ${offset}`;
+  //   }
+  @Get()
+  findAll() {
+    //@Res() response
+    // response.status(200).send('This action return all coffees');
+    return this.coffeeService.findAll();
+    return `This action return all coffees.`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    //@Param() params
+    //params.id
+    return this.coffeeService.findOne(id);
+
+    return `This action return #${id} coffee`;
+  }
+
+  @Post()
+  // Customize Http code
+  // @HttpCode(HttpStatus.GONE)
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    //@Body('name') body
+    return this.coffeeService.create(createCoffeeDto);
+    return createCoffeeDto;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeeService.update(id, updateCoffeeDto);
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
+    return `This action removes #${id} coffee`;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //filter out unwanted key values
+      forbidNonWhitelisted: true, //send error if  unwanted key values
+      transform: true, //tranform body into instance of DTO
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
