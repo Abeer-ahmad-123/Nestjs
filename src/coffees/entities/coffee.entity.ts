c7f02a94425ee7ad08cb7002e00c65640235387c
abeer-ahmad-123 <abeerahmad@devryx.com>
2023-11-02 13:16:30 +0500

    Lecture 31 -  Create a Relation between two Entities

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,24 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity/flavor.entity';
+
+@Entity('coffess') //represent sql table in lower case e.g coffee if you want custom name then enter inside Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+
+  @JoinTable()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
+  // @Column('json', { nullable: true }) //falvors is array and empty flavor is allowed
   flavors: string[];
 }
