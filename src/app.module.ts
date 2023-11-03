8ed79e78efd624abe2a927ea4bdddc070c02b499
abeer-ahmad-123 <abeerahmad@devryx.com>
2023-11-03 19:09:09 +0500

    Lecture 57 - Asynchronously Configure Dynamic Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,21 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      //we are using process evironment object loaded by ConfigModule module, now we are calling them before config module loads so all the value becomes undefined here
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, //disable in production
-    }),
+    TypeOrmModule.forRootAsync({
+      //we are using process evironment object loaded by ConfigModule module, now we are calling them before config module loads so all the value becomes undefined here --- forRoot and no useFactory
+      //
+      useFactory: () => ({
+        //behaves like any other asynchronous provider, it can inject dependencies thorugh inject property e.g we can inject ConfigService and use it here insted of using process.env
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true, //disable in production
+      }),
+    }), // our application is running because asynchronous configuration we added will be loaded after every module registered in application is resolved
     ConfigModule.forRoot({
       // envFilePath: '.env', // when deployed add ignoreEnvFile:true
       // validationSchema: Joi.object({
