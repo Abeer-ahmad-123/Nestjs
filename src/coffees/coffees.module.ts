685868ecbd0f8cc5428c2f9f83d4430910feef37
abeer-ahmad-123 <abeerahmad@devryx.com>
2023-11-03 13:00:05 +0500

    Lecture 44 - Class Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,11 +6,22 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity/flavor.entity';
 import { COFFEE_BRANDS } from './coggees-contants';
 
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService, // class based token
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
     { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }, //non-class based provider token
   ],
   exports: [CoffeesService], //exporting it to make public api so i can use it in coffee rating
