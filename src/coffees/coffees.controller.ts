dffa96bc803c58b659939807054ac508160c605e
abeer-ahmad-123 <abeerahmad@devryx.com>
2023-11-06 14:54:45 +0500

    Lecture 68 - Create Custom Param Decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,6 +15,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 // @UsePipes(ValidationPipe) //controller type
 // can apply instace of new class here but it will increase memory usage because nest can easily reuse instaces of same class here we are creating evertime when we will use the word new
@@ -35,9 +36,13 @@ export class CoffeesController {
   // @UsePipes(ValidationPipe) // method type
   @Public()
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(
+    @Protocol('https') protocol: string,
+    @Query() paginationQuery: PaginationQueryDto,
+  ) {
     //@Res() response
     // response.status(200).send('This action return all coffees');
+    console.log('protocol:', protocol);
     return this.coffeeService.findAll(paginationQuery);
     return `This action return all coffees.`;
   }
